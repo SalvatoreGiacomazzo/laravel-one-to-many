b8fc805ebbc565c1bb75446a82526d059426b0ad
SalvatoreGiacomazzo <giacomazzo161@gmail.com>
2024-11-07 23:02:38 +0100

    created foreign key

---
+++ laravel-new-exercise/database/migrations/2024_11_07_213152_add_device_id_foreign_key_to_wanted_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('wanted', function (Blueprint $table) {
+            $table->unsignedBigInteger("device_id")->after("id");
+
+            $table->foreign("device_id")->references("id")->on("wanted");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('wanted', function (Blueprint $table) {
+
+            $table->dropForeign("wanted_device_id_foreign");
+
+            $table->dropColumn("devide_id");
+        });
+    }
+};
